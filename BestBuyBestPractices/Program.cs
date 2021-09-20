458aa114498f3b1a7637ae060d98d0ad6c3fa908
Sean Morgan <seanmorgan@Seans-Air.attlocal.net>
2021-09-20 13:19:45 -0500

    complete department repository

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace BestBuyBestPractices
 {
@@ -6,7 +10,24 @@ namespace BestBuyBestPractices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+
+            var departments = repo.GetAllDepartments();
+
+            foreach(var dept in departments)
+            {
+                Console.WriteLine($"Department #    Department Name ");
+                Console.WriteLine($"{dept.DepartmentID}\t\t {dept.Name}");
+                Console.WriteLine($"--------------------------------");
+            }
         }
     }
 }
